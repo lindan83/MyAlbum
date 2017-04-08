a0c42d75458eafd1083e981ae3de6c004df31bc7
lindan <jackcoor@hotmail.com>
2017-04-08 10:22:32 +0800

    update

---
+++ app/src/main/java/com/lance/album/PhotoListActivity.java
@@ -0,0 +1,36 @@
+package com.lance.album;
+
+import android.view.View;
+
+import com.lance.common.widget.template.BaseActivity;
+
+/**
+ * 相册中的照片列表Grid
+ */
+public class PhotoListActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_photo_list;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initEventListeners() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void processClick(View view) {
+
+    }
+}
